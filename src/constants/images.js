1138a6d4108bf91d9ad19b235e78b5ce9b2fb9e3
Mihai Boroș <mihaiboros@Mihais-MacBook-Air.local>
2023-10-08 13:34:33 +0300

    image update

--- src/constants/images.js
+++ src/constants/images.js
@@ -23,7 +23,7 @@ import about02 from "../assets/about02.png";
 import about03 from "../assets/about03.png";
 import about04 from "../assets/about04.png";
 
-import profilv2 from "../assets/profilv2.png";
+import profilv2 from "../assets/profil_steve.png";
 import circle from "../assets/circle.svg";
 import logo from "../assets/logo.png";
 
